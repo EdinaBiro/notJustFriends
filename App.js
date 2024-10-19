0f383f0d875e41aca4cf38612b807d460fb709a8
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2024-10-19 15:29:23 +0300

    Adding component & clearing code

--- App.js
+++ App.js
@@ -1,91 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
-import LikeImage from './assets/images/like.png';
-import {
-  Entypo,
-  AntDesign,
-  FontAwesome5,
-  MaterialCommunityIcons,
-} from "@expo/vector-icons";
-
-
-
-const post = {
-  id: "p1",
-  createdAt: "19 m",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Vadim Savin",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
+import { StyleSheet,View} from 'react-native';
+import FeedPost from './src/components/FeedPost';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <View style={styles.post}>
-        {/*header*/}
-        <View style={styles.header}>
-          <Image source={ {uri: post.User.image}} style={styles.profileImage} />
-
-          <View>
-            <Text style={styles.name}>{post.User.name}</Text>
-            <Text style={styles.subtitle}>{post.createdAt}</Text>
-          </View>
-          <MaterialCommunityIcons name="dots-horizontal" size={18} color="gray" style={styles.icon}/>
-
-        </View>
-        {/*body*/}
-        {post.description && (
-          <Text style={styles.description}>{post.description}</Text>
-        )}
-
-        {post.image && (   //if conditional in jsx
-          <Image source={{uri: post.image}} style={styles.image}/>
-         
-         )}
-
-        {/*footer*/}
-        <View style={styles.footer}>
-          <View style={styles.statsRow}>
-            <Image source={LikeImage} style={styles.likeIcon}/>
-            <Text style={styles.likedBy}>Elon Musk and {post.numberOfLikes} others</Text>
-            <Text style={styles.numberOfShares}>{post.numberOfShares} shares</Text>
-
-          </View>
-
-          <View style={styles.buttonsRow}>
-            <View style={styles.iconButton}>
-              <AntDesign name ='like2' size={18} color="gray"/>
-              <Text style={styles.iconButtonText}>Like</Text>
-            </View>
-
-            <View style={styles.iconButton}>
-              <FontAwesome5 name ='comment-alt' size={18} color="gray"/>
-              <Text style={styles.iconButtonText}>Comment</Text>
-            </View>
-
-
-            <View style={styles.iconButton}>
-              <MaterialCommunityIcons name ='share-outline' size={18} color="gray"/>
-              <Text style={styles.iconButtonText}>Share</Text>
-            </View>
-          </View>
-
-        </View>
-
-      </View>
-
+      <FeedPost />
      
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -96,77 +21,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  profileImage:{
-    width: 40,
-    height:40,
-    borderRadius: 25,
-    marginRight: 10,
-  },
-  post:{
-    width: "100%",
-    marginVertical: 10,
-    backgroundColor: '#fff',
-  },
-  subtitle:{
-    color: "gray",
-  },
-  icon:{
-    marginLeft: "auto",
-  },
-  header:{
-    width: '100%',
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10,
-  },
-  name:{
-    fontWeight: "500",
-  },
-  image:{
-    width: '100%',
-    aspectRatio: 4/3,
-    marginTop: 10,
 
-  },
-  description:{
-    paddingHorizontal: 10,
-    lineHeight: 20,
-    letterSpacing: 0.3,
-
-  },
-  footer:{
-    paddingHorizontal: 10,
-  },
-  likeIcon:{
-    width: 20,
-    height: 20,
-    marginRight: 5,
-  },
-  statsRow:{
-    paddingVertical: 10,
-    flexDirection: "row",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: "lightgray",
-  },
-  likedBy:{
-    color: "gray",
-  },
-  shares:{
-    marginLeft: "auto'",
-    color: "gray",
-  },
-  buttonsRow:{
-    marginVertical: 10,
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  iconButton:{
-    flexDirection: "row",
-    aligItems: "center",
-  },
-  iconButtonText:{
-    marginLeft: 5,
-    color: "gray",
-    fontWeight: "500"
-  }
 });
