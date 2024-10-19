50a9014c0fb8103b41ab9a94f856e21665714f23
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2024-10-19 15:47:42 +0300

    Flatlist with dummy data

--- App.js
+++ App.js
@@ -1,62 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View} from 'react-native';
+import { StyleSheet,View,FlatList} from 'react-native';
 import FeedPost from './src/components/FeedPost';
+import posts from "./assets/data/posts.json";
 
-const post1 = {
-  id: "p1",
-  createdAt: "19 m",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Elon Musk",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  //image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
-
-const post2 = {
-  id: "p1",
-  createdAt: "19 m",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Zuck",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  //image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
-
-const post3 = {
-  id: "p1",
-  createdAt: "19 m",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Vadim Savin",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  //image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-      <FeedPost post={post1}/>
-      <FeedPost post={post2}/>
-      <FeedPost post={post3}/>
+      <FlatList 
+        data={posts}
+        renderItem={({item}) => <FeedPost post={item}/>}
+      />
+     
      
       <StatusBar style="auto" />
     </View>
@@ -67,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
   },
